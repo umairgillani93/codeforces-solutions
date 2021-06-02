d4015ea4ac75abb5d9e02bd9cee4aa298b053e7c
umairgillani93 <umairgillani93@gmail.com>
2021-06-03 02:51:51 +0500

    codeforces solutions

--- vasya_hipster.py
+++ vasya_hipster.py
@@ -1,15 +1,18 @@
-def main(r, b):
-    """vasya the hipster"""
+def main(r,b):
+    """vasya hipster challenge"
     low = min(int(r), int(b))
     high = max(int(r), int(b))
+
+    if low == high:
+        print(low, 0)
 
     high = high - low
     if high > 1:
         print(low, high//2)
 
-    elif high == 1:
-        print(low, high - 1)
+    if high == 1:
+        print(low, high-1)
 
-if __name__ == "__main__":
-    red, blue = (int(x) for x in input().split())
+if __name__ == '__main__':
+    red, blue = (int(x) for x in input.split())
     main(red, blue)
