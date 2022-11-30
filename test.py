e74003d8f1168001d5e7d03d730cc9a1d9f36047
umairgillani93 <umairgillani93@gmail.com>
2022-11-30 23:11:40 +0500

    new code added

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
+import os 
 import itertools
 
-combs = list(itertools.combinations(4, 2))
+n = int(input())
 
-print(combs)
+combs = list(itertools.combinations(range(n), 2))
+valids = []
 
+for x in combs:
+    if x[0] + x[1] == n:
+        valids.append((x[0], x[1]))
+
+    else:
+        continue
+
+def lcm(x, y):
+    for i in range(1, 11):
+        if x % i == 0 and y % i == 0:
+            return i
+        else:
+            continue
+
+for j in valids:
+    if lcm(j[0], j[1]) <= n:
+        print(j)
+    else:
+        continue
+            
