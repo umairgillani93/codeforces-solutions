d76e922e8fc97b9103602a015ca108658bd8c731
umairgillani93 <umairgillani93@gmail.com>
2022-12-27 22:39:12 +0500

    new code added

--- test.cpp
+++ test.cpp
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> squares(vector<int> vector) {
+vector<vector<int>> combs() {
+	vector<vector<int>> V;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i + j == n) {
+				vector<int> v;
+				v.push_back(i);
+				v.push_back(j);
+			}
+			else {
+				continue;
+			}
+		}
+		V.push_back(v);
+			
+	}	
 
-	vector<int> new_vector;
-	for (int i = 0; i < vector.size(); i++) {
-		new_vector.push_back(vector[i] * vector[i]);
+	for (int i = 0; i < V.size(); i++) {
+		cout << V[i] << '\n';
 	}
+	return V;
+	
 
-	for (int i = 0; i < vector.size(); i++) {
-		cout << new_vector[i] << '\n';
-	}
-	return new_vector;
-		
 }
 
-int main() {
-	vector<int> v;
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	squares();
-	return 0;
-}
--- test.py
+++ test.py
@@ -1,30 +1,11 @@
-import os 
-import itertools
+n = 10
+combs = []
 
-n = int(input())
-
-combs = list(itertools.combinations(range(n), 2))
-valids = []
-
-for x in combs:
-    if x[0] + x[1] == n:
-        valids.append((x[0], x[1]))
-
-    else:
-        continue
-
-def lcm(x, y):
-    for i in range(1, 11):
-        if x % i == 0 and y % i == 0:
-            return i
+for i in range(n):
+    for j in range(n):
+        if (i + j == n):
+            combs.append((i, j))
         else:
             continue
 
-for j in valids:
-    if lcm(j[0], j[1]) <= n:
-        print(j)
-    else:
-        continue
-
-print('new test line added')
-            
+print(combs)
