ce04d400a1a0b54df1cec0126fbc53c6f8dd60e3
umairgillani93 <umairgillani93@gmail.com>
2024-05-20 16:50:29 +0500

    new solutions

--- practice/t.cpp
+++ practice/t.cpp
@@ -6,37 +6,26 @@ int main() {
 	int t;
 	cin >> t;
 	while (t--) {
-		int n, k;
-		cin >> n >> k;
-		deque<int> dq(n);
+		int n, m, k;
+		cin >> n >> m >> k;
+		vector<int> b(n);
+		vector<int> c(m);
 		for (int i = 0; i < n; i++) {
-			cin >> dq[i];
+			cin >> b[i];
 		}
-		while (dq.size() > 1 && k) {
-			int m = min(dq.front(), dq.back());
-			if (k < 2 * m) {
-				// check if 2 * min is greater than k
-				// in this case we'll only process indivitual item
-				// and k value will be zero at the end to break the loop
-				dq.front() -= k / 2 + k % 2;
-				dq.back() -= k / 2;
-				k = 0;
-			}
-			else {
-				dq.front() -= m;
-				dq.back() -= m;
-				k -= 2 * m;
-			}
-			
-			if (dq.front() == 0) {
-				dq.pop_front();
-			}
-			if (dq.back() == 0) {
-				dq.pop_back();
+		for (int i = 0; i < m; i++) {
+			cin >> c[i];
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (b[i] + c[j] <= k) {
+					ans++;
+				}
 			}
 		}
-		int ans = n - dq.size();
-		cout << ans + (dq.size() && dq.front() <= k) << '\n';
+		cout << ans << '\n';
+		
 	}
 	return 0;
 }
