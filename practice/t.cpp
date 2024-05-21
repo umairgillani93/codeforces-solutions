0f2e50c7484e6f5a785a7906acdebac6b6c90423
umairgillani93 <umairgillani93@gmail.com>
2024-05-21 19:30:38 +0500

    time problem solution codeforces

--- practice/t.cpp
+++ practice/t.cpp
@@ -6,56 +6,32 @@ int main() {
 	int t;
 	cin >> t;
 	while (t--) {
-		int n;
-		cin >> n;
-		vector<int> arr(n);
-		for (int i = 0; i < n; i++) {
-			cin >> arr[i];
+		string s;
+		cin >> s;
+		string new_s = "";
+		for (int i = 0; i < 2; i++) {
+			new_s += s[i];
 		}
-		int mx_ele = *max_element(arr.begin() + 1, arr.end() - 1);
-		if (arr[0] >= mx_ele || arr[n - 1] >= mx_ele) {
-			cout << "NO" << '\n';
+		long long first = stoll(new_s);
+
+		if (first == 12) {
+			cout << first << ":" << s[3] << s[4] << " " << "PM" << '\n';
+			continue;
+		}
+		if (first > 12) {
+			first -= 12;
+			cout << to_string(first) << ":" << s[3] << s[4] << " " << "PM" << '\n';
+			continue;
+		}
+
+		if (first == 00) {
+			cout << 12 << ":" << s[3] << s[4] << " " << "AM" << '\n';
 			continue;
 		}
 		else {
-			auto Max = [&](vector<int> &arr) {
-				int mx_i = 1;
-				int mx = arr[1];
-				for (int i = 02 i < arr.size() - 1; i++) {
-					if (arr[i] >= mx) {
-						mx_i = i;
-						mx = arr[i];
-					}
-				}
-				return mx_i + 1; 
-			};
-
-			auto AllZero = [&](vector<int> &arr) {
-				for (int i = 0; i < arr.size(); i++) {
-					if (arr[i] != 0) {
-						return false;
-					}
-				}
-				return true;
-			};
-			bool done = false;
-			while (!done) {
-				auto mx_i = Max(arr);
-				arr[mx_i] -= 2;
-				arr[mx_i - 1]--;
-				arr[mx_i + 1]--;
-				if (arr[mx_i] < 0 || arr[mx_i + 1] < 0 || arr[mx_i - 1] < 0) {
-					cout << "NO" << '\n';
-				}
-				else {
-					auto zero_check = AllZeros(arr);
-					if (zero_check) {
-
-					}
-				}
-			}
-		
+			cout << s << " " << "AM" << '\n';
 		}
+
 	}
 	return 0;
 }
---
+++ practice/t.py
@@ -0,0 +1,16 @@
+t = int(input())
+for _ in range(t):
+    s = str(input())
+    if s[:2] == "12":
+        print(s + " " + "PM")
+    elif int(s[:2]) > 12:
+        new = int(s[:2]) - 12
+        if len(str(new)) < 2:
+            new = str(0) + str(new)
+        print(str(new) + ":" + s.split(":")[1] + " " + "PM")
+    elif s[:2] == "00":
+        print(str(12) + ":" + s.split(":")[1] + " " + "AM")
+    
+    else:
+        print(str(s) + " " + "AM")
+    
