727b490989e1f2084f4663915bf7869b9107a12d
umairgillani93 <umairgillani93@gmail.com>
2024-05-26 01:53:14 +0500

    codeforces solution

---
+++ practice/entertainment_mac.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string t = s;
+        reverse(t.begin(), t.end());
+        cout << min(s, t + s) << '\n';
+    }
+
+    return 0;
+}
+
--- practice/sol.cpp
+++ practice/sol.cpp
@@ -3,45 +3,17 @@
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    int b = 0;
-	vector<int> arr;
-
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '(') {
-            b++;
-        }
-        else {
-            b--;
-        }
-		arr.push_back(b);
-    }
-	int l = s.length() - 1;
-	arr.erase(arr.begin() + l);
-	arr.insert(arr.begin() + 0, 0);
-	
-	 //for (auto c: arr) {
-	 //	cout << c << " ";
-	 //}
-	 //cout << '\n';
-
-	vector<pair<int, int>> res;
-	for (int i = 0; i < s.length(); i++) {
-		pair<int, int> p;
-		p.first = arr[i];
-		p.second = i + 1;
-		res.emplace_back(p);
-	}
-	sort(res.begin(), res.end(), [&](pair<int, int> &a, pair<int, int> &b) {
-			return a.first < b.first;
-			});
-
-	for (auto it = res.begin(); it != res.end(); it++) {
-		cout << it -> first << " " << it -> second << '\n';
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		string t = s;
+		reverse(t.begin(), t.end());
+		cout << min(s, t + s) << '\n';
 	}
 
-
-    return 0;
+	return 0;
 }
-
