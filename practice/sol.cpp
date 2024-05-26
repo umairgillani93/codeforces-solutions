78b086b80dfba37d4b4171d1d4db0798e753aa84
umairgillani93 <umairgillani93@gmail.com>
2024-05-26 23:44:51 +0500

    turtle math solution

--- practice/sol.cpp
+++ practice/sol.cpp
@@ -8,12 +8,21 @@ int main() {
 	while (t--) {
 		int n;
 		cin >> n;
-		string s;
-		cin >> s;
-		string t = s;
-		reverse(t.begin(), t.end());
-		cout << min(s, t + s) << '\n';
-	}
+		vector<int> arr(n);
+		for (int i = 0; i < n; i++) {
+			cin >> arr[i];
+			arr[i]--;
+		}
 
+		int ans = 3;
+		for (int i = 0; i < arr.size() - 1; i++) {
+			if ((arr[i] == i + 1) && (arr[i + 1] == i)) {
+				ans = 2;
+				break;
+			}
+		}
+		cout << ans << '\n';
+	}
 	return 0;
 }
+
