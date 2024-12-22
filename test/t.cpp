751d6a970ceda04e57cdf92fe7966485c273c91a
umairgillani93 <umairgillani93@gmail.com>
2024-12-23 00:46:25 +0500

    new problem div3_A

---
+++ test/t.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a = 2;
+	int b = 2;
+	int k = 1;
+	cout << __gcd(a, b) << '\n';
+	cout << a / 2 << " " << b / 2 << '\n';
+
+	return 0;
+}
