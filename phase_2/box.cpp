9b5de4e471eabf9443cebe8151c793cb54936b4d
umairgillani93 <umairgillani93@gmail.com>
2024-12-29 18:27:10 +0500

    TLE CP sheet first problem 800

---
+++ phase_2/box.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		if (is_sorted(a.begin(), a.end()) || k > 1) {
+			cout << "YES" << '\n';
+		}
+		else {
+			cout << "NO" << '\n';
+		}
+	}
+	return 0;
+}
